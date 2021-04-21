3d824a1019d1e23318e86c35f9314602463fdd45
JLP603 <60498189+JLP603@users.noreply.github.com>
2021-04-21 13:38:43 +0800

    formatted some stuff again

--- CSOPESY-Project1-S15-Group 41.c
+++ CSOPESY-Project1-S15-Group 41.c
@@ -26,24 +26,18 @@ found.” error message and then terminates
 */
 #include <stdio.h>
 #include <stdlib.h> // For exit() function
-void PreemptSJF(int y){
+void PreemptSJF(FILE *fptr, int y){
       int limit=y;
-      int arrival_time[10], burst_time[10], temp[10];
-      int i, smallest, count = 0, time, limit;
+      int process_id[100], arrival_time[100], burst_time[100], temp[100];
+      int i, smallest, count = 0, time;
       double wait_time = 0, turnaround_time = 0, end;
       float average_waiting_time, average_turnaround_time;
-      printf("\nEnter the Total Number of Processes:\t");
-      scanf("%d", &limit); 
-      printf("\nEnter Details of %d Processesn", limit);
-      for(i = 0; i < limit; i++)
+      for(int i=0;i<limit;i++)
       {
-            printf("\nEnter Arrival Time:\t");
-            scanf("%d", &arrival_time[i]);
-            printf("Enter Burst Time:\t");
-            scanf("%d", &burst_time[i]); 
+            fscanf(fptr, "%d %d %d\n", &process_id[i], &arrival_time[i], &burst_time[i]);
             temp[i] = burst_time[i];
       }
-      burst_time[9] = 9999;  
+      burst_time[99] = 9999;  
       for(time = 0; count != limit; time++)
       {
             smallest = 9;
@@ -132,7 +126,7 @@ int main() {
             
             if(x==2)
             {
-              PreemptSJF(y);
+              PreemptSJF(fptr, y);
             }
             fclose(fptr);
       }
