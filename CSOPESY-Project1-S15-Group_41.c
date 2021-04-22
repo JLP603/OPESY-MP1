fb7451bc91b3aca1c34b93acd79de7fc1bde6339
Subsonic-2000 <71876891+Subsonic-2000@users.noreply.github.com>
2021-04-22 19:43:09 +0800

    Update CSOPESY-Project1-S15-Group_41.c

--- CSOPESY-Project1-S15-Group_41.c
+++ CSOPESY-Project1-S15-Group_41.c
@@ -30,15 +30,14 @@ found.” error message and then terminates
 
 void nonPreSJF (FILE *fptr, int y){
 	int i, j, total=0, pos, temp, limit = y, zero = 0;
-	int arrival_time[100], burst_time[100], process_id[100], wait_time[100], ta_time[100];
+	int arrival_time[100], burst_time[100], process_id[100], wait_time[100], ta_time[100], start_time[100], comp_time[100];
 	float avg_wait_time;
 	
 	for(i = 0; i < limit; i++){
         fscanf(fptr, "%d %d %d", &process_id[i], &arrival_time[i], &burst_time[i]);
     }
     
-    //sorting of arrival times so that the process with an arrival time of 0 comes first
-    
+    //sorting of arrival times so that the process with an arrival timem of 0 comes first
     
     for(i = 0; i < limit; i++){
         pos = i;
@@ -62,7 +61,7 @@ void nonPreSJF (FILE *fptr, int y){
         arrival_time[pos] = temp;
     }
     
-	//Now we will sort the burst times
+	//sorting of burst times
     for(i = zero; i < limit; i++){
         pos = i;
         for(j = i+1; j < limit; j++){
@@ -81,30 +80,31 @@ void nonPreSJF (FILE *fptr, int y){
         arrival_time[pos] = temp;
     }
     
-    wait_time[0] = 0;
+    start_time[0] = 0;
 	          
     for(i = 1; i < limit; i++){
-        wait_time[i] = 0;
+        start_time[i] = 0;
         for(j = 0; j < i; j++){
-            wait_time[i] += burst_time[j];
+            start_time[i] += burst_time[j];
         }
-        total += wait_time[i];
     }
     
-    avg_wait_time = (float)total/limit;      
-    total = 0;
+    //ta_time = comp_time - arrival_time && wait_time = ta_time - burst_time
     
     for(i = 0; i < limit; i++){
-        ta_time[i] = burst_time[i] + wait_time[i];   
-        total += ta_time[i];
+    	comp_time[i] = start_time[i] + burst_time[i];
+    	ta_time[i] = comp_time[i] - arrival_time[i];
+    	wait_time[i] = ta_time[i] - burst_time[i];
+        total += wait_time[i];
         printf("P[%d]\n", process_id[i]);
-		printf("Start time: %d   End time: %d\n", wait_time[i], wait_time[i] + burst_time[i]);
+		printf("Start time: %d   End time: %d\n", start_time[i], comp_time[i]);
+		//printf("Burst time: %d\n", burst_time[i]);
 		printf("Waiting time: %d\n", wait_time[i]);
 		printf("Turn around time: %d\n", ta_time[i]);
 		printf("********************\n\n");
     }
-    
-    printf("Average Waiting Time=%f", avg_wait_time);
+    avg_wait_time = (float)total/limit;
+    printf("Average Waiting Time = %f", avg_wait_time);
 }
 
 void PreemptSJF(FILE *fptr, int y){
